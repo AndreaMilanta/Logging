418820af36246ed4bb03ffbf8415e1f84f38308c
Andrea Milanta <andrea.milanta@gmail.com>
2018-06-19 18:09:20 +0200

    added delegate for video logging

--- source/LoggerManager.cs
+++ source/LoggerManager.cs
@@ -14,12 +14,14 @@ namespace Logging
     {
         private static LoggerManager _instance = null;
 
+        public delegate void InstantLogMethod(string logEvent);
+
         public string FolderPath { get; private set; }
         public string Culture { get; private set; }
         public string DateFormat { get; private set; }
         public string NameFormat { get; private set; }
         public int TimerDt{ get; private set; }
-        public bool ToConsole { get; private set; }
+        public InstantLogMethod InstantLog { get; private set; }
         public bool Enabled { get; private set; }
         public List<string> Logs { get => logDict.Keys.ToList(); }
         public static LoggerManager Instance { get => _instance == null ? throw new LogManagerNotInitiliazedException() : _instance; }
@@ -28,7 +30,7 @@ namespace Logging
 
         private Dictionary<string, Logger> logDict;
 
-        private LoggerManager(string folderPath, bool enabled ,bool toConsole, int dt, string nameFormate, string dateFormat, string culture)
+        private LoggerManager(string folderPath, bool enabled ,int dt, string nameFormate, string dateFormat, string culture, InstantLogMethod instantLog = null)
         {
             //Set instance as singleton
             _instance = this;
@@ -42,7 +44,7 @@ namespace Logging
             this.DateFormat = dateFormat;
             this.NameFormat = nameFormate;
             this.TimerDt = dt;
-            this.ToConsole = toConsole;
+            this.InstantLog = instantLog;
             this.Enabled = enabled;
 
             if (!Enabled)
@@ -65,13 +67,13 @@ namespace Logging
             timer.Start();
         }
 
-        public static LoggerManager SetupLoggerManager(string folderPath, bool enabled = true, bool toConsole = LogConstants.TO_CONSOLE,
-                                                       int timerDt = LogConstants.TIMER_DT_s, string nameFormat = LogConstants.NAME_FORMAT,
-                                                       string dateFormat = LogConstants.DATE_FORMAT, string culture = LogConstants.CULTURE)
+        public static LoggerManager SetupLoggerManager(string folderPath, bool enabled = true, int timerDt = LogConstants.TIMER_DT_s,
+                                                       string nameFormat = LogConstants.NAME_FORMAT, string dateFormat = LogConstants.DATE_FORMAT,
+                                                       string culture = LogConstants.CULTURE, InstantLogMethod instantLog = null)
         {
             if (_instance != null)
                 throw new LogManagerAlreadyInitiliazedException();
-            return new LoggerManager(folderPath, enabled, toConsole, timerDt, nameFormat, dateFormat, culture);
+            return new LoggerManager(folderPath, enabled, timerDt, nameFormat, dateFormat, culture, instantLog);
         }
 
         public void RegisterLogger(string logName, string dateFormat = null)
@@ -99,9 +101,8 @@ namespace Logging
                 return;
             try
             {
-                if (this.ToConsole)
-                    Console.WriteLine(logEvent);
                 logDict[log].Log(logEvent);
+                this.InstantLog?.Invoke(logEvent);
             }
             catch (KeyNotFoundException)
             {
@@ -116,8 +117,7 @@ namespace Logging
             try
             {
                 logDict[log].Log(excEvent);
-                if (this.ToConsole)
-                    Console.WriteLine(excEvent);
+                this.InstantLog?.Invoke(excEvent);
             }
             catch (KeyNotFoundException)
             {
@@ -132,8 +132,7 @@ namespace Logging
             try
             {
                 logDict[log].Log(ex.ToString());
-                if (this.ToConsole)
-                    Console.WriteLine(ex.ToString());
+                this.InstantLog?.Invoke(ex.ToString());
             }
             catch (KeyNotFoundException)
             {
